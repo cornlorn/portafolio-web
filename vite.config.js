bab3094e34f7e17d744cb52484c025e1df34b796
Pablo Sosa <sosadavid4280@outlook.com>
2024-11-02 19:27:03 -0600

    Minificación y Purga

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from "vite";
+import purgecss from "vite-plugin-purgecss";
+import cssnanoPlugin from "cssnano";
 
 export default defineConfig({
   base: "/portafolio-web/",
+  plugins: [
+    purgecss({
+      content: ["./**/*.{html,js}"],
+    }),
+  ],
   css: {
+    postcss: {
+      plugins: [
+        cssnanoPlugin({
+          preset: "default",
+        }),
+      ],
+    },
     preprocessorOptions: {
       less: {},
     },
